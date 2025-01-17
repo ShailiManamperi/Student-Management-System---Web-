7e5ae0017cde39142a4904bfe33bca0ef5023fa7
Shaili Manamperi <119599080+ShailiManamperi@users.noreply.github.com>
2025-01-17 09:07:43 +0530

    Update MainNav.js

--- Front-End/src/components/templetes/MainNav.js
+++ Front-End/src/components/templetes/MainNav.js
@@ -8,30 +8,30 @@ import logo from '../../assets/logo1.jpg';
 export default function Navbar() {
   return (
     <div>
-  <nav
-    className="navbar navbar-expand-lg fixed-top"
-    style={{ backgroundColor: '#0a1f23', color: '#ffffff'}}
-  >
-    <div className="container-fluid">
-      {/* Logo and Branding */}
-      <div className="d-flex align-items-center flex-wrap flex-lg-nowrap w-100">
-        <div className="d-flex align-items-center flex-grow-1">
-          <img
-            src={logo}
-            alt="Logo"
-            style={{ width: '40px', height: '40px' , borderRadius:'20px' }}
-            className="me-2"
-          />
-          <span
-            className=" text-white fs-6 fs-md-4"
-            style={{ lineHeight: '1.2' }}
-          >
-            The U-Dot Information
-          </span>
+      <nav
+        className="navbar navbar-expand-lg fixed-top"
+        style={{ backgroundColor: '#0a1f23', color: '#ffffff'}}
+      >
+        <div className="container-fluid">
+          {/* Logo and Branding */}
+          <div className="d-flex align-items-center flex-wrap flex-lg-nowrap w-100">
+            <div className="d-flex align-items-center flex-grow-1">
+              <img
+                src={logo}
+                alt="Logo"
+                style={{ width: '40px', height: '40px' , borderRadius:'20px' }}
+                className="me-2"
+              />
+              <span
+                className=" text-white fs-6 fs-md-4"
+                style={{ lineHeight: '1.2' }}
+              >
+                The U-Dot Information
+              </span>
+            </div>
+          </div>
         </div>
-      </div>
+      </nav>
     </div>
-  </nav>
-</div>
   );
 }
